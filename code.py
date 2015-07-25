0b6170e0af4bb5796c3d230f6047729ad6947c4e
Liza Lichtinger <SL@lizas-mbp.gateway.sonic.net>
2015-07-25 16:29:14 -0700

    code update to gitty

--- code.py
+++ code.py
@@ -1,1 +1,21 @@
-d 
+# data set directions for writing to csv file from .arff
+# import for fast easy and expressive data structure
+
+import pandas as pd
+import numpy as np
+import arff
+import urllib2
+import matplotlib.pyplot as plt
+import re
+
+# importing library ^^ for plot production and interactive 2D data viz
+
+from sklearn.ensemble import RandomForestClassifier
+%matplotlib inline
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import (classification_report, confusion_matrix, roc_curve, auc,
+                             accuracy_score, roc_auc_score)
+
