2eb99b3bce8a02cc05cc8210edcfce8d02d0f1e3
Liza Lichtinger <WhiteMt@users.noreply.github.com>
2015-09-27 15:45:19 -0700

    updating code

--- code.py
+++ code.py
@@ -19,19 +19,19 @@ from sklearn.metrics import (classification_report, confusion_matrix, roc_curve,
 
 //#loading data with path to file directions and naming dataset
 
-data_arff = arff.load(open ('Dataset.arff', 'rb'))
+//#data_arff = arff.load(open ('Dataset.arff', 'rb'))
 //# direction ^^ to load data, now stored in dict
 
-column_names = [x[0] for x in data_arff['attributes']]
-//# direction ^^ to get column names by calling key 'attributes' getting first valuein each tuple
+//#column_names = [x[0] for x in data_arff['attributes']]
+//# direction ^^ to get column names by calling key 'attributes' getting first value in each tuple
 
-df = pd.DataFrame(data_arff['data'], columns = column_names)
+//#df = pd.DataFrame(data_arff['data'], columns = column_names)
 //# direction ^^ to load data into pandas data frame && set column names
 
-df = df.astype(int)
+//#df = df.astype(int)
 //# direction ^^ to change column types from 'object' to 'int'
 
-df.Result = df.Result.map(lambda x: 0 if x <= -1 else 1)
+//#df.Result = df.Result.map(lambda x: 0 if x <= -1 else 1)
 //# dict 1=phishing -1=nonphishing
 
 //#load data into csv format
